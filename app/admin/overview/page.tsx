f2b4defbbab6335767c77443da791556833e3b08
tfavro <wetr9902@gmail.com>
2025-12-02 21:57:20 -0600

    fixing deployment error

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,11 +1,6 @@
 import { auth } from "@/auth";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-
-import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
-import Link from "next/link";
-import Charts from "./charts";
+
 
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
